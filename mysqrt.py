85f554d95ff826f85788329e2d88950d361f0644
Nickolaus Mueller <nickolaus@gmail.com>
2014-04-23 20:26:29 -0400

    added convergence

--- mysqrt.py
+++ mysqrt.py
@@ -1,7 +1,13 @@
 x = 9.
 s = 1.
-for k in range(6):
+kmax = 100
+tol = 1.e-14
+for k in range(kmax):
 	print "Before iteration %s, s= %s" % (k,s)
+	s0=s
 	s = 0.5 * (s + x/s)
+	delta_s = s - s0
+	if abs(delta_s/x) < tol:
+		break
 print "After %s iterations, s=%s" % (k+1,s)
 
